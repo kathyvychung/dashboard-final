70e609a3a2dd8c99d3825cfdedb09e7eb19c893d
kathyvychung <kvce33@drexel.edu>
2020-11-15 17:34:23 -0500

    nodes

--- dev/js/demo.js
+++ dev/js/demo.js
@@ -1,25 +1,25 @@
-// import {gsap} from "gsap";
+import {gsap} from "gsap";
 
-
+gsap.set("#neck",{transformOrigin: "center"})
 /* -----------------
     Demo Script
 ----------------- */
+const giraffeTL = gsap.timeline();
+giraffeTL.from("#neck",{duration: 3, y: 600, ease: "none"}, "swing")
+        .from("#first",{duration: 2, alpha:0}, "swing")
+        .from("#second",{duration: 2, alpha:0, delay: 1}, "swing")
+        .from("#third",{duration: 2, alpha:0, delay: 2}, "swing")
+        .from("#tail",{duration:1, rotation:20, transformOrigin:"left top", repeat:3, ease: "none"}, "swing")
+        .to("#tail",{duration:1, rotation:0, transformOrigin:"left top", repeat:3, ease: "none"}, "swing")
+        .to("#part1",{ duration: 0.1, alpha: 0})
+        .from("#part2",{duration: 0.1, alpha:0})
+        .from("#neck2",{duration: 3, y: 600},"rise")
+        .from("#leaf1",{duration: 3, y: 600},"rise")
+        .from("#leaf2",{duration: 3, y: 600},"rise")
+        .from("#completion",{duration: 2.5, y: 500},"rise")
+        .from("#ear2",{rotation:20, transformOrigin:"left top"});
 
-// var timeOfAnimation = 2;
 
-// var boxAnimationTL = gsap.timeline();
-
-// boxAnimationTL.to(".stick",{duration: timeOfAnimation, rotation: 360, x:500, y:400})
-
-//             .from(".circle-1",{duration: timeOfAnimation, x:-800, y:300})
-//             .to(".circle-1",{duration: timeOfAnimation, x:800, y:300})
-
-//             .from(".circle-2",{duration: timeOfAnimation, x:-700, y:100})
-//             .to(".circle-2",{duration: timeOfAnimation, x:700, y:100},"-=0.5")
-
-//             .from(".circle-3",{duration: timeOfAnimation, x:-550, y:-100})
-//             .to(".circle-3",{duration: timeOfAnimation, x:550, y:-100},"-=0.5")
-
-//             .to(".circle-3",{duration: 1, alpha:0})
-//             .to(".circle-2",{duration: 1, alpha:0})
-//             .to(".circle-1",{duration: 1, alpha:0});
+export function giraffeAnimation(){
+    return giraffeTL;
+}
--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -1,20 +1,13 @@
-// import * as Demo from './demo.js';
+import * as Demo from './demo.js';
 import {gsap} from "gsap";
+// import { GSDevTools } from "gsap/GSDevTools";
 
-import {stickAnimation} from "./stick.js"
-import {greenCircleAnimation} from "./greenCircle.js"
-import {whiteCircleAnimation} from "./whiteCircle.js"
-import {pinkCircleAnimation} from "./pinkCircle.js"
-import {whiteDisappear} from "./whiteDisappear"
-import {greenDisappear} from "./greenDisappear"
-// console.log(Demo);
+// gsap.registerPlugin(GSDevTools);
 
-const mainTL = gsap.timeline({paused:true});
+import {giraffeAnimation} from "./demo.js"
 
-mainTL.add(stickAnimation())
-    .add(greenCircleAnimation())
-    .add(whiteCircleAnimation())
-    .add(pinkCircleAnimation())
-    .add(whiteDisappear())
-    .add(greenDisappear())
-    .play();
+const mainTL = gsap.timeline();
+mainTL.add(giraffeAnimation);
+
+//instantiate GSDevTools with default settings
+// GSDevTools.create();
