05e5de17c8dc2d649805db669fb5f7d30d37ec5a
kathyvychung <kvce33@drexel.edu>
2020-11-17 21:34:45 -0500

    loading screen

--- dev/js/dashboard.js
+++ dev/js/dashboard.js
@@ -8,11 +8,9 @@ const dashboardTL = gsap.timeline();
                 .to("#outside-thickring",{duration: 1, drawSVG:"100%", ease:"none"},"ring")
                 .from("#inside-ring",{duration: 1, drawSVG:"0%", ease:"none", alpha: 0},"ring")
                 .from("#bars",{duration: 0.5, alpha: 0, rotation: 340, transformOrigin:"middle"},"bars")
-                .from("#R",{duration: 3, alpha: 0},"bars")
-                .fromTo("#scan-4",{duration: 1, drawSVG:"0% 30%"}, {drawSVG:"100% 100%", ease:"none", repeat: 2, alpha: 0},"load")
-                .fromTo("#scan-3",{duration: 1, drawSVG:"0% 30%"}, {drawSVG:"100% 100%", ease:"none", repeat: 2 },"load")
-                .fromTo("#scan-2",{duration: 1, drawSVG:"0% 30%"}, {drawSVG:"100% 100%", ease:"none", repeat: 2 },"load")
-                .fromTo("#scan-1",{duration: 1, drawSVG:"0% 30%"}, {drawSVG:"100% 100%", ease:"none", repeat: 2 },"load");
+                .from("#R",{duration: 1, alpha: 0},"bars")
+                .from(".scan",{duration: 2, alpha: 0, drawSVG:"0%"},"load")
+                .fromTo(".extra",{duration: 2, drawSVG:"0% 30%", alpha: 0}, {duration: 2, drawSVG:"100% 100%", ease:"none", repeat: 2, alpha: 1},"load");
 
 export function dashboardAnimation(){
     return dashboardTL;
