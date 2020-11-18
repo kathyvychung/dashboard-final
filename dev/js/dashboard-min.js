8abc7c7cc6bb135ecad4436dbc33873ee5c337a1
kathyvychung <kvce33@drexel.edu>
2020-11-17 21:28:13 -0500

    logo animation

--- dev/js/dashboard-min.js
+++
@@ -1,1 +0,0 @@
-import{gsap}from"gsap";import{DrawSVGPlugin}from"gsap/DrawSVGPlugin";gsap.registerPlugin(DrawSVGPlugin);const dashboardTL=gsap.timeline();export function dashboardAnimation(){return dashboardTL}
--- dev/js/dashboard.js
+++ dev/js/dashboard.js
@@ -4,15 +4,15 @@ import { DrawSVGPlugin } from "gsap/DrawSVGPlugin";
 gsap.registerPlugin(DrawSVGPlugin);
 
 const dashboardTL = gsap.timeline();
-    dashboardTL.fromTo("#outside-thinring",{duration: 1, drawSVG:"30% -90%"}, {drawSVG:"100% 70%", ease:"none"})
-                .to("#outside-thinring",{duration: 1, drawSVG:"100%", ease:"none"})
-                .from("#inside-ring",{duration: 1, drawSVG:"100%", ease:"none"})
+    dashboardTL.fromTo("#outside-thickring",{duration: 1, drawSVG:"0% 30%"}, {drawSVG:"70% 100%", ease:"none"})
+                .to("#outside-thickring",{duration: 1, drawSVG:"100%", ease:"none"},"ring")
+                .from("#inside-ring",{duration: 1, drawSVG:"0%", ease:"none", alpha: 0},"ring")
                 .from("#bars",{duration: 0.5, alpha: 0, rotation: 340, transformOrigin:"middle"},"bars")
-                // .from("#Path-2",{duration: 0.5, alpha: 0, drawSVG:"50% 50%", ease:"none"},"bars")
-                // .from("#Path-2-Copy-2",{duration: 0.5, alpha: 0, drawSVG:"50% 50%", ease:"none"},"bars")
-                // .from("#Path-2-Copy",{duration: 0.5, alpha: 0, drawSVG:"50% 50%"},"bars")
-                .to("#loadingScan",{duration: 1, drawSVG:"100%", ease:"none"})
-                .from("#R",{duration: 1, alpha: 0},"bars");
+                .from("#R",{duration: 3, alpha: 0},"bars")
+                .fromTo("#scan-4",{duration: 1, drawSVG:"0% 30%"}, {drawSVG:"100% 100%", ease:"none", repeat: 2, alpha: 0},"load")
+                .fromTo("#scan-3",{duration: 1, drawSVG:"0% 30%"}, {drawSVG:"100% 100%", ease:"none", repeat: 2 },"load")
+                .fromTo("#scan-2",{duration: 1, drawSVG:"0% 30%"}, {drawSVG:"100% 100%", ease:"none", repeat: 2 },"load")
+                .fromTo("#scan-1",{duration: 1, drawSVG:"0% 30%"}, {drawSVG:"100% 100%", ease:"none", repeat: 2 },"load");
 
 export function dashboardAnimation(){
     return dashboardTL;
