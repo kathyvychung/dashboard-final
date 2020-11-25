944eb05c15955e13f56d9383daf4ced6b42492c6
kathyvychung <kvce33@drexel.edu>
2020-11-24 20:24:00 -0500

    update dashboard

--- dev/js/dashboard.js
+++ dev/js/dashboard.js
@@ -6,6 +6,7 @@ gsap.registerPlugin(DrawSVGPlugin);
 const dashboardTL = gsap.timeline();
 
 export function dashboardAnimation(){
-    dashboardTL.to("#buttons",{duration: 3, alpha:0, delay:5});
+    dashboardTL.to("#buttons",{duration: 1, alpha:0, delay:5})
+                .to("#dashboardLine",{duration: 1, drawSVG:"100%", ease:"none"});
     return dashboardTL;
 }
--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -8,8 +8,8 @@ gsap.registerPlugin(GSDevTools);
 
 const mainTL = gsap.timeline();
 
-mainTL.add(loadingScreenAnimation)
-    .add(dashboardAnimation);
+mainTL.add(loadingScreenAnimation())
+    .add(dashboardAnimation());
 
 //instantiate GSDevTools with default settings
 GSDevTools.create();
