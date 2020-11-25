1a763c657acd8e6c52b7b43a2173ccf0fb3c2fe4
kathyvychung <kvce33@drexel.edu>
2020-11-24 22:44:21 -0500

    map

--- dev/js/dashboard.js
+++ dev/js/dashboard.js
@@ -6,7 +6,12 @@ gsap.registerPlugin(DrawSVGPlugin);
 const dashboardTL = gsap.timeline();
 
 export function dashboardAnimation(){
-    dashboardTL.to("#buttons",{duration: 1, alpha:0, delay:5})
-                .to("#dashboardLine",{duration: 1, drawSVG:"100%", ease:"none"});
+    dashboardTL.from("#controlCenter",{duration: 1, y:1000},"first")
+                .to("#dashboardLine",{duration: 1, drawSVG:"100%", ease:"none"},"first")
+                .from("#borderCPLeft",{duration: 1, x: 1000},"first")
+                .from("#borderCPRight",{duration: 1, x: -1000},"first")
+                .from("#topText",{duration: 1, alpha: 0},"first")
+                .from("#map",{duration: 1, delay: 2, x:-300},"first");
+        
     return dashboardTL;
 }
