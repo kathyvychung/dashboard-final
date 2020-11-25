4a31b5dd094ae896ebdfa53c7d4599675ca4a8c1
kathyvychung <kvce33@drexel.edu>
2020-11-25 09:28:26 -0500

    map

--- dev/js/dashboard.js
+++ dev/js/dashboard.js
@@ -13,10 +13,12 @@ export function dashboardAnimation(){
                 .from("#map",{duration: 1, x:-300},"second")
                 .from(".fuelTank",{duration: 1, alpha: 0, stagger: 1},"second")
                 .from(".wifiTank",{duration: 1, alpha: 0, stagger: 1},"second")
+                .from("#mph",{duration: 1, alpha: 0},"second")
                 .from("#outer-ring",{duration: 3, alpha: 0, rotation: 360, transformOrigin: "50% 50%"},"second")
                 .from("#inner-ring",{duration: 3, alpha: 0, rotation: -360, transformOrigin: "50% 50%"},"second")
                 .from("#hexagon",{duration: 1, drawSVG:"0%", ease:"none", alpha: 0},"first")
                 .from(".gauges",{duration: 1, drawSVG:"0%", ease:"none", alpha: 0, stagger: 1},"second")
+                .from("#circleGauges",{duration: 1, alpha: 0, stagger: 1},"second")
                 .from("#orange-load",{duration: 1, rotation: 360, transformOrigin: "50% 0%", alpha: 0, stagger: 1},"second");
         
     return dashboardTL;
