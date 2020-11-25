a2b794db0a1d7f1ca53bc36aa39989b6241597e3
kathyvychung <kvce33@drexel.edu>
2020-11-25 00:42:57 -0500

    control center/dials

--- dev/js/dashboard.js
+++ dev/js/dashboard.js
@@ -7,11 +7,17 @@ const dashboardTL = gsap.timeline();
 
 export function dashboardAnimation(){
     dashboardTL.from("#controlCenter",{duration: 1, y:1000},"first")
-                .to("#dashboardLine",{duration: 1, drawSVG:"100%", ease:"none"},"first")
                 .from("#borderCPLeft",{duration: 1, x: 1000},"first")
                 .from("#borderCPRight",{duration: 1, x: -1000},"first")
                 .from("#topText",{duration: 1, alpha: 0},"first")
-                .from("#map",{duration: 1, delay: 2, x:-300},"first");
+                .from("#map",{duration: 1, x:-300},"second")
+                .from(".fuelTank",{duration: 1, alpha: 0, stagger: 1},"second")
+                .from(".wifiTank",{duration: 1, alpha: 0, stagger: 1},"second")
+                .from("#outer-ring",{duration: 3, alpha: 0, rotation: 360, transformOrigin: "50% 50%"},"second")
+                .from("#inner-ring",{duration: 3, alpha: 0, rotation: -360, transformOrigin: "50% 50%"},"second")
+                .from("#hexagon",{duration: 1, drawSVG:"0%", ease:"none", alpha: 0},"first")
+                .from(".gauges",{duration: 1, drawSVG:"0%", ease:"none", alpha: 0, stagger: 1},"second")
+                .from("#orange-load",{duration: 1, rotation: 360, transformOrigin: "50% 0%", alpha: 0, stagger: 1},"second");
         
     return dashboardTL;
 }
