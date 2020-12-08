3cee7b1f9fe2d1244e544bd4745e85d0f7688aff
kathyvychung <kvce33@drexel.edu>
2020-12-07 22:19:34 -0500

    tm

--- dev/js/timeMachine.js
+++ dev/js/timeMachine.js
@@ -9,6 +9,7 @@ export function timeMachineAnimation(){
     timeMachineTL.to("#clock-icon",{duration: 0.5, stroke:"#2DD4D6", fill:"#2DD4D6" },"fourth")
                 .to("#dashboard",{duration: 2, y:-3000})
                 .from("#time-machine",{duration: 2, y:3000},"-=3");
+                .from(".gauges",{duration: 1, drawSVG:"0%", ease:"none", alpha: 0, stagger: 1},"second")
         
     return timeMachineTL;
 }
